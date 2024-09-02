bc72ea85b6a86ead601f884cd927501c7b926bc8
kaiquecandido77 <146864018+kaiquecandido77@users.noreply.github.com>
2024-09-02 20:12:53 -0300

    Add files via upload

---
+++ Array.php
@@ -0,0 +1,11 @@
+<?php
+$estado =[
+    "SP" => "Sao Paulo",
+    "RJ" => "Rio de Janeiro",
+    "MG" => "Belo Horizonte",
+    "RS" => "Porto Alegre",
+    "BA" => "Salvador",
+];
+
+print_r($estado);
+?>
